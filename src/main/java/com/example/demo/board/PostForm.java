3116559f4375d3d600bdc62d13c51924b950e25c
minwoo park <pmw9027@outlook.kr>
2020-04-14 02:55:10 +0900

    Added APIs of Board

---
+++ src/main/java/com/example/demo/board/PostForm.java
@@ -0,0 +1,15 @@
+package com.example.demo.board;
+
+public class PostForm {
+
+    String title;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
