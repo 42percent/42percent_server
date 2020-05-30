d5365cf24c55ea127c0b68d06655d995c161d36b
unknown <tmdrn457@hs.ac.kr>
2020-05-30 16:24:07 +0900

    feature(#109)-Add Controllers

--- src/main/java/com/example/demo/RestAPIController.java
+++ src/main/java/com/example/demo/RestAPIController.java
@@ -1,7 +1,28 @@
 package com.example.demo;
 
+import org.hibernate.annotations.SelectBeforeUpdate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestAPIController {
+
+    @RequestMapping(value = "/user/login")
+    public String Login_info_get(@RequestParam("username") String username, String password){
+        System.out.println("아이디 :" +username);
+        System.out.println("패스워드 :" + password);
+        return "login";
+    }
+
+    @RequestMapping(value = "/user/signup")
+    public String info_for_login(@RequestParam("username") String username, String name) {
+        System.out.println("아이디 :" + username);
+        System.out.println("아이11디 :" + name);
+        return "signup";
+    }
+
+    public interface PageMapper{
+        
+    }
 }
